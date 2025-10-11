3121e55b0d88b7c0ed98fa6122a42058212749a2
MindaugasBalakauskas <mindaugas.balakauskas@gmail.com>
2025-10-11 15:52:41 +0100

    Initial Commit

---
+++ StateManagement/StateManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateManagementApp.swift
+//  StateManagement
+//
+//  Created by M Balakauskas on 11/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct StateManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
