248f3f5d3a0775125af240339b62172e230d9369
MindaugasBalakauskas <mindaugas.balakauskas@gmail.com>
2025-10-21 12:57:38 +0100

    update

--- StateManagement/Model/TodoItem.swift
+++ StateManagement/Model/TodoItem.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct TodoItem: Codable, Identifiable {
+struct TodoItem: Hashable, Codable, Identifiable {
     var id: UUID = UUID()
     var title: String
     var description: String?
--- StateManagement/Views/TodoItemsListView.swift
+++ StateManagement/Views/TodoItemsListView.swift
@@ -12,18 +12,37 @@ struct TodoItemsListView: View {
     @StateObject var viewModel = TodoItemsListViewModel()
     
     var body: some View {
-        NavigationView {
+        NavigationStack {
             List {
                 ForEach($viewModel.todoItems) { $todoItem in
-                    ToDoItemRow(item: $todoItem.onNewValue {
-                        viewModel.reorder()
-                    })
+                    NavigationLink(value: todoItem) {
+                        ToDoItemRow(item: $todoItem.onNewValue {
+                            viewModel.reorder()
+                        })
+                    }
+//                   NavigationLink {
+//                      TodoItemDetailView(item: $todoItem)
+//                      } label: {
+//                        ToDoItemRow(item: $todoItem.onNewValue {
+//                              viewModel.reorder()
+//                         })
+//                   }
                 }
                 .onDelete(perform: viewModel.deleteItems(at:))
                 .onMove(perform: viewModel.moveItems(from:to:))
             }
             .listStyle(.insetGrouped)
             .navigationTitle("Today's tasks")
+            .navigationDestination(for: TodoItem.self, destination: { todoItem in
+                let todoItemBinding = Binding(
+                    get: { viewModel.todoItems.first(where: { $0.id == todoItem.id})! },
+                    set: { newItem in
+                        let index = viewModel.todoItems.firstIndex(where: {$0.id == todoItem.id})!
+                        viewModel.todoItems[index] = newItem
+                    }
+                )
+                TodoItemDetailView(item: todoItemBinding)
+            })
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     EditButton()
