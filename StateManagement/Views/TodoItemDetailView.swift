3c7a837304225e42f647e35611180a4d6ddb9e02
MindaugasBalakauskas <mindaugas.balakauskas@gmail.com>
2025-10-21 10:35:32 +0100

    app restructure

---
+++ StateManagement/Views/TodoItemDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  TodoItemDetailView.swift
+//  StateManagement
+//
+//  Created by M Balakauskas on 21/10/2025.
+//
+
+import SwiftUI
+
+struct TodoItemDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoItemDetailView()
+}
